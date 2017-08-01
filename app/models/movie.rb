0fa29624704ef22471d79a91b5e4193043e6c823
Becca Smith <rsmith71@chicagobooth.edu>
2017-08-01 15:33:47 +0000

    1033 am

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -17,11 +17,10 @@ validates :duration, :numericality => { :only_integer => true, :greater_than_or_
  #- description: no rules
  #- image_url: no rules
 
- belongs_to(:director, :class_name => "Director", :foreign_key =>
- "director_id")
+belongs_to(:director, :class_name => "Director", :foreign_key => "director_id")
     
-  has_many(:characters, :class_name => "Character", :foreign_key => "movie_id") 
+has_many(:characters, :class_name => "Character", :foreign_key => "movie_id") 
   
-  has_many(:actor, :class_name => "Actor", :foreign_key => "actor")
+has_many(:actor, :class_name => "Actor", :foreign_key => "actor")
   
 end
